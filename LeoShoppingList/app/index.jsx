88504bd45e101015b9bb0ca7b5ecc315b3217482
LeonardoA <vas22soler@gmail.com>
2026-05-05 12:26:20 -0300

    Terceira Etapa do Projeto

--- LeoShoppingList/app/index.jsx
+++ LeoShoppingList/app/index.jsx
@@ -5,8 +5,20 @@ export default function Welcome() {
   return (
     <View style={styles.container}>
       <View style={styles.containerImage}>
-          1
+          <image 
+            source={require('../assets/avatar.png')}
+            style={styles.avatar} 
+          />
        </View>
+        <View style={styles.content}>
+          <Text style={style.title}>Leo Shopping List</Text>
+          <Text style={styles.text}>
+            Monte sua lista de compras e não esqueça mais o que precisa comprar ao sair de casa!!!
+          </Text>
+          <link style={styles.button} href={"/home"}>
+            <Text style={styles.buttonText}>Acessar</Text>
+          </link>
+        </View>
     </View>
   );
 }
@@ -15,7 +27,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#00000',
+  },
+  containerImage: {
+    flex: 2 ,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  avatar: {
+    width: 230,
+    height: 230,
+    resizeMode: 'cover',
+    borderRadius: 115,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#dadada',
+    borderTopStartRadius: 25,
+    borderTopEndRadius:25,
+    paddingHorizontal: '5%'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight:'bold',
+    marginVertical:20,
+    textAlign:'center'
+  },
+  text:{
+    fontSize: 16,
+    color: 'gray'
+  },
+  button:{
+    position: 'absolute',
+    backgroundColor: '#000',
+    bottom: '15%',
+    alignSelf:'center',
+    borderRadius: 50,
+    paddingVertical:15,
+    width:'60%',
+    textAlign:'center'
+  },
+  buttonText:{
+    fontSize:22,
+    color:'#fff',
+    fontWeight:'bold'
+  }
 });
