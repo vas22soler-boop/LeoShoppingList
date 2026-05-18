248da10c773d6dd99e4a8ea6e186d168af41ec3c
LeonardoA <vas22soler@gmail.com>
2026-05-18 07:58:21 -0300

    Quinta Etapa do Projeto

--- LeoShoppingList/app/home.jsx
+++ LeoShoppingList/app/home.jsx
@@ -1,46 +1,99 @@
 import React from 'react'
-import {  Alert, ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { 
+  Alert,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function Home() {
 
-    function addProduto() {
-        Alert.alert('Adiconar Produto')
-    }
+  function addProduto() {
+    Alert.alert("Adicionar Produto");
+  }
+
   return (
-    <SafeAreaView style={{ felx: 1, backgroundColor: '#000'}}>
-        <ImageBackground
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+      <ImageBackground
         source={require('../assets/background.jpg')}
         resizeMode='repeat'
         style={{ flex: 1, justifyContent: 'flex-start'}}
-        >
-            <View style={styles.header}>
-                <Text style={styles.title}>Lista de Compras</Text>
-                <Ionicons name='trash' size={32} color='#fff'></Ionicons>
-            </View>
+      >
+        <View style={styles.header}>
+          <Text style={styles.title}>Lista de Compras</Text>
+          <Ionicons name='trash' size={32} color="#fff"></Ionicons>
+        </View>
 
-            {/*Lista de compras */}
+        {/* Lista de compras */}
 
-            <View style={styles.footer}>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                    color='#fff'
-                    fontSize={18}
-                    placeholder='Digite o nome do produto...'
-                    placeholderTextColor='#aeaeae'
-                    />
-                </View>
-                <TouchableOpacity style= {styles.iconContainer}>
-                    <Ionicons name="add" size={36} color='fff'/>
-                </TouchableOpacity>
-            </View>
+        <View style={styles.footer}>
+          <View style={styles.inputContainer} onPress>
+            <TextInput
+              color="#fff"
+              fontSize={18}
+              placeholder='Digite o nome do produto...'
+              placeholderTextColor="#aeaeae"
+            />
+          </View>
+          <TouchableOpacity style={styles.iconContainer} >
+            <Ionicons name="add" size={36} color="#fff" />
+          </TouchableOpacity>
+        </View>
 
-
-        </ImageBackground>
-
+      </ImageBackground>
     </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    header: {
+        padding: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignContent: 'center',
+        backgroundColor:'#000000c0',
+        borderBottomStartRadius: 30,
+        borderBottomEndRadius: 30,
+    },
+    title:{
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    footer:{
+        position: 'absolute',
+        bottom:0,
+        width:'100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal:20,
+        backgroundColor:'#000000c0',
+        borderTopStartRadius:30,
+        borderTopEndRadius:30,
+    },
+    inputContainer:{
+        backgroundColor:'#000000',
+        elevation: 40,
+        flex: 1,
+        heigth: 50,
+        marginVertical: 20,
+        borderRadius: 30,
+        justifyContent:'center,'
+
+    },
+    iconContainer:{
+        borderRadius: 25,
+        heigth:50,
+        width: 50,
+        backgroundColor: '#000000',
+        elevation: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+})
